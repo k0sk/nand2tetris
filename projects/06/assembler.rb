801bc2cd05c8e691aa9ea042a6851619fbe6e48c
kosk <mail@kosk.me>
2016-08-06 16:15:24 +0900

    Implement Assembler

--- projects/06/assembler.rb
+++ projects/06/assembler.rb
@@ -4,20 +4,54 @@ load 'lib/symbol_table.rb'
 
 filename = ARGV[0]
 
+
+# First path
+rom_address = 0
 parser = Parser.new(filename)
+table = SymbolTable.new()
 
 while parser.has_more_commands?
   parser.advance
-  if parser.command_type == CommandType::A_COMMAND
-    puts "0#{format('%.15b', parser.symbol)}"
+
+  if parser.command_type == CommandType::C_COMMAND ||
+      parser.command_type == CommandType::A_COMMAND
+    rom_address += 1
   elsif parser.command_type == CommandType::L_COMMAND
-    # puts "symbol: #{parser.symbol}"
-  else
-    parser.parse
-    puts "111#{Code.comp(parser.comp)}#{Code.dest(parser.dest)}" \
-            "#{Code.jump(parser.jump)}"
-    # puts "dest: #{parser.dest} #{Code.dest(parser.dest)}"
-    # puts "comp: #{parser.comp} #{Code.comp(parser.comp)}"
-    # puts "jump: #{parser.jump} #{Code.jump(parser.jump)}"
+    unless table.contains(parser.symbol)
+      table.add_entry(parser.symbol, rom_address)
+    end
   end
 end
+
+
+# Second path
+ram_address = 16
+parser = Parser.new(filename)
+output_file = File.new(filename.gsub('asm', 'hack'), 'w')
+
+while parser.has_more_commands?
+  parser.advance
+
+  if parser.command_type == CommandType::C_COMMAND
+    parser.parse
+    output_file.write("111#{Code.comp(parser.comp)}#{Code.dest(parser.dest)}" \
+                        "#{Code.jump(parser.jump)}\n")
+  elsif parser.command_type == CommandType::A_COMMAND
+    if (true if Float(parser.symbol) rescue false) # If symbol is number
+      symbol = parser.symbol.to_i
+    else
+      if table.contains(parser.symbol)
+        symbol = table.get_address(parser.symbol)
+      else
+        table.add_entry(parser.symbol, ram_address)
+        symbol = ram_address
+
+        ram_address += 1
+      end
+    end
+
+    output_file.write("0#{format('%.15b', symbol)}\n") unless symbol.nil?
+  end
+end
+
+output_file.close()
