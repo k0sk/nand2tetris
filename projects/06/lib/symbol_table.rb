5cf87a882b3e540ab5c356caec83b4efc26299c7
kosk <mail@kosk.me>
2016-08-06 13:51:24 +0900

    Implement SymbolTable class

--- projects/06/lib/symbol_table.rb
+++ projects/06/lib/symbol_table.rb
@@ -1,14 +1,17 @@
 class SymbolTable
   def initialize()
+    @table = {}
   end
 
-  def add_entry()
+  def add_entry(symbol, address)
+    @table[symbol] = address
   end
 
-  def contains()
+  def contains(symbol)
+    return @table.has_key?(symbol)
   end
 
-  def get_address()
+  def get_address(symbol)
+    return @table[symbol]
   end
 end
-
